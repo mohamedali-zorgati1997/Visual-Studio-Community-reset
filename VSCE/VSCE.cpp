98d697162238ae64defd7f5b9bd4f2fb732bbfa3
mohamedali-zorgati1997 <46978677+mohamedali-zorgati1997@users.noreply.github.com>
2020-03-29 21:54:35 +0100

    cleened old testing comments

--- VSCE/VSCE.cpp
+++ VSCE/VSCE.cpp
@@ -5,44 +5,6 @@
 
 int main()
 {
-    //std::cout << "Hello World!\n"; 
-
-	/*
-	DATA_BLOB input;
-	DATA_BLOB inagain;
-	DATA_BLOB output;
-	BYTE data[] = "Hello World!!!!!!!!";
-	input.pbData = data;
-	input.cbData = sizeof(data);
-	CryptProtectData(&input, NULL, NULL, NULL, NULL, CRYPTPROTECT_LOCAL_MACHINE, &output);
-	CryptUnprotectData(&output, NULL, NULL, NULL, NULL, CRYPTPROTECT_LOCAL_MACHINE, &inagain);
-	printf("%s\n\n", inagain.pbData);
-	unsigned char Data [2000];
-	HKEY hkey = HKEY_CLASSES_ROOT;
-	DWORD X = 2000;
-	LPDWORD size = &X;
-	*/
-	//LONG xx = RegGetValue(HKEY_CLASSES_ROOT,
-	//L"Licenses\\41717607-F34E-432C-A138-A3CFD7E25CDA\\09278",
-	//L"", RRF_RT_REG_BINARY, nullptr,(PVOID) Data, size);
-	/*printf("return %d\n", xx);
-	printf("size=%d\n", *size);
-	for (int i=0; i < 200; i++) {
-		printf("%x\n", Data[i]);
-	}
-	printf("\n%s\n", Data);
-	output.pbData = Data;
-	output.cbData = *size;
-	CryptUnprotectData(&output, NULL, NULL, NULL, NULL, CRYPTPROTECT_LOCAL_MACHINE, &inagain);
-	printf("size=%d\n", (inagain.cbData));
-	for (int i = 0; i < (inagain.cbData); i++) {
-		printf("%x\t", inagain.pbData[i]);
-	}
-	inagain.pbData[inagain.cbData - 14]--;
-	printf("\n[%x]\n", inagain.pbData[inagain.cbData - 14]);
-	CryptProtectData(&inagain, NULL, NULL, NULL, NULL, CRYPTPROTECT_LOCAL_MACHINE, &output);
-	*/
-
 	time_t sec, val = 1;
 	struct tm* current_time;
 	current_time = (struct tm*)malloc(sizeof(struct tm));
@@ -95,21 +57,4 @@ int main()
 	else {
 		printf("Failed to open Registry key!\n");
 	}
-
-
-
-	/*
-	if (RegOpenKeyExW(HKEY_CLASSES_ROOT, L"Licenses\\41717607-F34E-432C-A138-A3CFD7E25CDA\\09278", 0, KEY_SET_VALUE, &hkey) == ERROR_SUCCESS) {
-		if (RegSetValueExW(hkey, L"", 0, REG_BINARY, output.pbData, output.cbData) == ERROR_SUCCESS) {
-			printf("change happened\n");
-		}
-		else {
-			printf("Key not changed! Error : %d", (unsigned int)GetLastError());
-		}
-		RegCloseKey(hkey);
-	}
-	else {
-		printf("Key Opening Failed!\n");
-	}*/
-
 }
